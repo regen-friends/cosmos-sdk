82d72de4f16a3ff802050e26e0627160f06917d5
Sahith Reddy Narahari <sahith@vitwit.com>
2019-11-01 19:16:38 +0530

    added comments for debugging

--- x/upgrade/abci.go
+++ x/upgrade/abci.go
@@ -2,6 +2,7 @@ package upgrade
 
 import (
 	"fmt"
+	"log"
 
 	abci "github.com/tendermint/tendermint/abci/types"
 
@@ -15,20 +16,23 @@ import (
 // The prupose is to ensure the binary is switch EXACTLY at the desired block, and to allow
 // a migration to be executed if needed upon this switch (migration defined in the new binary)
 func BeginBlocker(k Keeper, ctx sdk.Context, _ abci.RequestBeginBlock) {
+	log.Printf("This is Begin blocker")
 	plan, found := k.GetUpgradePlan(ctx)
-	if found {
-		if plan.ShouldExecute(ctx) {
-			if !k.HasHandler(plan.Name) {
-				upgradeMsg := fmt.Sprintf("UPGRADE \"%s\" NEEDED at %s: %s", plan.Name, plan.DueAt(), plan.Info)
-				// We don't have an upgrade handler for this upgrade name, meaning this software is out of date so shutdown
-				ctx.Logger().Error(upgradeMsg)
-				panic(upgradeMsg)
-			}
-			// We have an upgrade handler for this upgrade name, so apply the upgrade
-			ctx.Logger().Info(fmt.Sprintf("applying upgrade \"%s\" at %s", plan.Name, plan.DueAt()))
-			ctx = ctx.WithBlockGasMeter(sdk.NewInfiniteGasMeter())
-			k.ApplyUpgrade(ctx, plan)
+	if !found {
+		return
+	}
+	if plan.ShouldExecute(ctx) {
+		if !k.HasHandler(plan.Name) {
+			upgradeMsg := fmt.Sprintf("UPGRADE \"%s\" NEEDED at %s: %s", plan.Name, plan.DueAt(), plan.Info)
+			// We don't have an upgrade handler for this upgrade name, meaning this software is out of date so shutdown
+			ctx.Logger().Error(upgradeMsg)
+			panic(upgradeMsg)
 		}
+		// We have an upgrade handler for this upgrade name, so apply the upgrade
+		ctx.Logger().Info(fmt.Sprintf("applying upgrade \"%s\" at %s", plan.Name, plan.DueAt()))
+		ctx = ctx.WithBlockGasMeter(sdk.NewInfiniteGasMeter())
+		k.ApplyUpgrade(ctx, plan)
+		return
 	}
 
 	// if we have a pending upgrade, but it is not yet time, make sure we did not
--- x/upgrade/module.go
+++ x/upgrade/module.go
@@ -2,9 +2,13 @@ package upgrade
 
 import (
 	"encoding/json"
+	"fmt"
+	"log"
 
 	"github.com/gorilla/mux"
 	"github.com/spf13/cobra"
+
+	abci "github.com/tendermint/tendermint/abci/types"
 
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/context"
@@ -13,7 +17,6 @@ import (
 	"github.com/cosmos/cosmos-sdk/types/module"
 	"github.com/cosmos/cosmos-sdk/x/upgrade/client/cli"
 	"github.com/cosmos/cosmos-sdk/x/upgrade/client/rest"
-	abci "github.com/tendermint/tendermint/abci/types"
 )
 
 // module codec
@@ -125,6 +128,8 @@ func (am AppModule) ExportGenesis(ctx sdk.Context) json.RawMessage {
 //
 // CONTRACT: this is registered in BeginBlocker *before* all other modules' BeginBlock functions
 func (am AppModule) BeginBlock(ctx sdk.Context, req abci.RequestBeginBlock) {
+	fmt.Errorf("this is begin block of upgrade")
+	log.Fatalf("this is begin block of upgrade")
 	BeginBlocker(am.keeper, ctx, req)
 }
 
